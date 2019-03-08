ad63cf6a37f2ada6a010e6e6d66594e28423d6a1
Gaurav soni <Gaurav@allcadservices.com>
2019-03-08 16:51:08 +0400

    updated test case

--- tests/routes/attendee.test.js
+++ tests/routes/attendee.test.js
@@ -1,18 +1,21 @@
 const request = require('supertest');
 const app = require('../../app/app');
 const Attendee = require('../../app/models/attendee.model');
+const mongoose =  require('mongoose');
 
-beforeEach((done)=>{
+beforeEach(()=>{
   Attendee.deleteMany();
-  done();
 });
 
-test('Should create a new attendee',(done)=>{
-  request(app).post('/attendee/create').send({
+test('Should create a new attendee',async ()=>{
+  await request(app).post('/attendee/create').send({
     "name":"max",
     "age":50,
     "occupation":"accounts",
     "businessName":"Dxb"
   }).expect(200)
-  done();
+});
+
+afterAll(() => {
+  mongoose.disconnect();
 });
