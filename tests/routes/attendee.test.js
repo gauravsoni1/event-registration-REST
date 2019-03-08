f389c4a82988b2c62cf45e01c7f332dc5a809611
Gaurav soni <Gaurav@allcadservices.com>
2019-03-08 15:59:53 +0400

    updated the test cases

--- tests/routes/attendee.test.js
+++ tests/routes/attendee.test.js
@@ -2,15 +2,17 @@ const request = require('supertest');
 const app = require('../../app/app');
 const Attendee = require('../../app/models/attendee.model');
 
-beforeEach(async ()=>{
-  await Attendee.deleteMany();
+beforeEach((done)=>{
+  Attendee.deleteMany();
+  done();
 });
 
-test('Should create a new attendee',async ()=>{
-  await request(app).post('/attendee/create').send({
+test('Should create a new attendee',(done)=>{
+  request(app).post('/attendee/create').send({
     "name":"max",
     "age":50,
     "occupation":"accounts",
     "businessName":"Dxb"
   }).expect(200)
+  done();
 });
